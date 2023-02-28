aadd20d9383085a3701df86c4809408e26a28fda
serhasHacizade <eren.felek.4@gmail.com>
2023-02-28 12:49:25 +0300

    commit

--- useContext/src/contexts/ThemeContext.js
+++ useContext/src/contexts/ThemeContext.js
@@ -1,10 +1,18 @@
-import {createContext, useState} from 'react'
+import {createContext, useEffect, useState} from 'react'
 
 //this component use createContext cause this component will called by every component
 export const ThemeContext = createContext();
+//this line set default theme value for useEffect
+const defaultTheme = localStorage.getItem("theme") || "light";
  
 export const ThemeContextProvider = ({children}) => {
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState(defaultTheme);
+
+    //we write default theme in local storage cause every screen repetition change the theme value
+    
+    useEffect(() =>{
+        localStorage.setItem("theme", theme)
+    },[theme])
     
     //you can call this function where you want 
     const toggleTheme = () => {
