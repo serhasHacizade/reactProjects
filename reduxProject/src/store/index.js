7443b44b861a620d2f61a9c76c10056460fdb93c
serhasHacizade <eren.felek.4@gmail.com>
2023-04-24 11:39:45 +0300

    commit

--- reduxProject/src/store/index.js
+++ reduxProject/src/store/index.js
@@ -3,9 +3,13 @@ import {createStore} from "redux";
 const counterReducer = (state = {counter : 0}, action) => {
     if (action.type === "increment") {
         return {counter : state.counter + 1};
-    } else if (action.type === decrement) {
-        return {counter : state.counter - 1};
+    } 
+    if (action.type === "increase") {
+        return {counter : state.counter + action.amount};
     }
+    if (action.type === "decrement") {
+        return {counter : state.counter - 1};
+    } 
     return state;
 };
 
