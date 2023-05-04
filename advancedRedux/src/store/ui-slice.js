904cb741a8c77e52d80b720a48e4745bb0903e1f
serhasHacizade <eren.felek.4@gmail.com>
2023-05-04 15:54:55 +0300

    commit

--- advancedRedux/src/store/ui-slice.js
+++ advancedRedux/src/store/ui-slice.js
@@ -1,17 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {cartIsVisible : false, notification : null};
+const initialState = { cartIsVisible: false, notification: null };
 const uiSlice = createSlice({
-    name : "ui",
-    initialState : initialState,
-    reducers : {
-        toggle(state) {state.cartIsVisible = !state.cartIsVisible},
+    name: "ui",
+    initialState: initialState,
+    reducers: {
+        toggle(state) { state.cartIsVisible = !state.cartIsVisible },
         showNotification(state, action) {
             state.notification = {
-                status : action.payload.status,
-                title : action.payload.title,
-                message : action.payload.message
-            }}
+                status: action.payload.status,
+                title: action.payload.title,
+                message: action.payload.message
+            }
+        }
     },
 });
 export const uiActions = uiSlice.actions;
