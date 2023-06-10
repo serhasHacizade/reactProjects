c137890ad8bd433acc763a9b505a17e599d2750b
serhasHacizade <eren.felek.4@gmail.com>
2023-06-10 15:44:24 +0300

    commit

--- replaceRedux/src/containers/Favorites.js
+++ replaceRedux/src/containers/Favorites.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
-import FavoriteItem from '../components/Favorites/FavoriteItem';
-import './Products.css';
+import FavoriteItem from "../components/Favorites/FavoriteItem";
+import "./Products.css";
 
 const Favorites = (props) => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
+  const favoriteProducts = useSelector((state) =>
+    state.shop.products.filter((p) => p.isFavorite)
   );
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
       <ul className="products-list">
-        {favoriteProducts.map(prod => (
+        {favoriteProducts.map((prod) => (
           <FavoriteItem
             key={prod.id}
             id={prod.id}
@@ -25,5 +25,4 @@ const Favorites = (props) => {
   }
   return content;
 };
-
 export default Favorites;
