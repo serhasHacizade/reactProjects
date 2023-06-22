2a27c170e2927aab1f0442c9a54ebc5c0f9ec81b
serhasHacizade <eren.felek.4@gmail.com>
2023-06-22 16:32:41 +0300

    commit

--- replaceRedux/src/App.js
+++ replaceRedux/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-
 import Navigation from "./components/Nav/Navigation";
 import ProductsPage from "./containers/Products";
 import FavoritesPage from "./containers/Favorites";
